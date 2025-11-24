6e096192a6fcf03e74d59d214de51f94432f39d4
Taiwo Afusat <taiwo.asat@gmail.com>
2025-11-24 13:32:17 +0100

    ALX Counter assignment completed

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,19 +2,22 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile';
 import Footer from './components/Footer';
+import Counter from './components/Counter';  // <-- Correct import
 
 function App() {
   return (
     <>
       <Header />
 
-      <MainContent>
-        <UserProfile
-          name="Taiwo Afusat"
-          age={25}
-          bio="I love React, frontend development, and exploring new cities."
-        />
-      </MainContent>
+      <MainContent />
+      
+      <UserProfile
+        name="Taiwo Afusat"
+        age={25}
+        bio="I love React, frontend development, and exploring new cities."
+      />
+
+      <Counter />
 
       <Footer />
     </>
